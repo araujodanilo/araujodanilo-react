8b6d5c9dc28e90fe77be2de95c2b7ab0175d3ffb
Danilo Araujo <x45danilo45x@gmail.com>
2024-03-16 02:45:36 -0300

    feat: create footer component

---
+++ src/components/layout/footer/index.tsx
@@ -0,0 +1,9 @@
+export function FooterComponent() {
+  return (
+    <footer className=" bg-zinc-300/75 dark:bg-zinc-800/25 w-full rounded-md px-10 py-4 transition-colors duration-200">
+      <span className="text-zinc-700 dark:text-zinc-50/75 text-sm flex justify-center">
+        Developed with ❤️ by Danilo Araujo.
+      </span>
+    </footer>
+  );
+}
