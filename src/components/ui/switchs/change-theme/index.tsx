913e38ce90aa8116543f73491aa9ee4cb93c91d1
Danilo Araujo <x45danilo45x@gmail.com>
2024-03-16 07:35:39 -0300

    fix: icon now show with theme is system

--- src/components/ui/switchs/change-theme/index.tsx
+++ src/components/ui/switchs/change-theme/index.tsx
@@ -1,4 +1,4 @@
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, SunMoon } from "lucide-react";
 
 import { useTheme } from "../../../../contexts/theme-provider";
 
@@ -8,9 +8,9 @@ export function ChangeThemeSwitch() {
   return (
     <>
       <span className="md:mr-5 my-auto hover:bg-zinc-50 dark:bg-zinc-800 hover:dark:bg-zinc-900 p-1 rounded-full transition-colors duration-200">
-        {/* {theme === "system" && (
+        {theme === "system" && (
           <SunMoon className="size-6 stroke-yellow-600 dark:stroke-violet-500" onClick={changeTheme} />
-        )} */}
+        )}
         {theme === "dark" && <Moon className="size-6" onClick={changeTheme} />}
         {theme === "light" && <Sun className="size-6" onClick={changeTheme} />}
       </span>
