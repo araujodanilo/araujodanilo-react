d0dee956565438fb309503637d19cce2c6947b25
Danilo Araujo <x45danilo45x@gmail.com>
2024-03-16 06:27:18 -0300

    feat: adding logic on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,20 @@
+import { HomeCardComponent } from "../components/cards/home-card";
+import { HomeCardsInfo } from "../constants/home-info";
+
 export function HomePage() {
   return (
     <>
-      <h1>Home</h1>
+      <div className="flex flex-col gap-10 pb-5 w-full md:w-9/12 mx-auto">
+        {HomeCardsInfo.map((info, index) => (
+          <HomeCardComponent
+            imageSide={index % 2 === 0 ? "left" : "right"}
+            title={info.title}
+            paragraphs={info.paragraphs}
+            image={info.image}
+            key={index}
+          />
+        ))}
+      </div>
     </>
   );
 }
