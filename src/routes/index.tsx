d51517f9dc96aaa57c0a37a19262b55aa8df4640
Danilo Araujo <x45danilo45x@gmail.com>
2024-03-16 06:26:56 -0300

    feat: link not found page on routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { HomePage } from "../pages/home";
 import { Layout } from "../components/layout";
+import { NotFoundPage } from "../pages/not-found";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route element={<Layout />} path="/">
         <Route path="/" Component={HomePage} />
-        <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="*" Component={NotFoundPage} />
       </Route>
     </Routes>
   );
