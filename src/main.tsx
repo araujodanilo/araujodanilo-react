68f0cc53bb88cd89bf57395ac45a415ac9a9c1c8
Danilo Araujo <x45danilo45x@gmail.com>
2024-03-15 21:19:44 -0300

    feat: create a project with vite

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <>
+      <h1>App</h1>
+    </>
+  </React.StrictMode>
+);
